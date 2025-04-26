fb8d83fbf3029333d079208644c282db6ea25210
M-Alhaig <mohammedalhaij100@gmail.com>
2025-04-26 14:00:02 +0300

    Batman

---
+++ src/test/java/com/mordizze/linkshortner/link_shortner/LinkShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mordizze.linkshortner.link_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
