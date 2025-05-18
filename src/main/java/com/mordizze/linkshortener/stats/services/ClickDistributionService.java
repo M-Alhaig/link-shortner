935fd8f4ca4cf27d8643a5623c7fd49b02e5e806
M-Alhaig <mohammedalhaij100@gmail.com>
2025-05-18 23:05:18 +0300

    Refactor ClickDistributionService to use LinkedHashMap for maintaining order in hourly and daily click distributions. Update daily distribution to use full day names instead of enum names for better readability. Streamline event processing by merging hourly and daily counts into respective maps.

--- src/main/java/com/mordizze/linkshortener/stats/services/ClickDistributionService.java
+++ src/main/java/com/mordizze/linkshortener/stats/services/ClickDistributionService.java
@@ -2,10 +2,11 @@ package com.mordizze.linkshortener.stats.services;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -34,8 +35,8 @@ public class ClickDistributionService {
         LocalDateTime after = LocalDateTime.now().minusWeeks(INTERVAL_WEEKS);
         List<ClickEvents> events = clickEventsRepo.findByClickedAtAfterAndLinkOrderByClickedAt(after, link);
 
-        Map<String, Long> hourlyDistribution = new TreeMap<>();
-        Map<String, Long> dailyDistribution = new TreeMap<>();
+        Map<String, Long> hourlyDistribution = new LinkedHashMap<>();
+        Map<String, Long> dailyDistribution = new LinkedHashMap<>();
 
         for (int i=0; i < 24; i++) {
             String formated = String.format("%02d", i);
@@ -43,31 +44,42 @@ public class ClickDistributionService {
         }
 
         for (DayOfWeek day : DayOfWeek.values()) {
-            dailyDistribution.put(day.name(), 0L);
+            dailyDistribution.put(day.getDisplayName(TextStyle.FULL, Locale.ENGLISH), 0L);
         }
+
 
         events.stream().map(event -> {
             LocalDateTime dateTime = event.getClickedAt();
-            String formated = String.format("%02d", dateTime.getHour());
-            return formated;
-        }).collect(Collectors.groupingBy(
-            hour -> hour,
-            Collectors.counting()
-        ))
-        .forEach((hour, count) -> {
-            if (hourlyDistribution.containsKey(hour))
-                hourlyDistribution.put(hour, count);
+            String formatedHour = String.format("%02d", dateTime.getHour());
+            String day = event.getClickedAt().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+            return new String[] {formatedHour, day};
+        }).forEach((arr) -> {
+            hourlyDistribution.merge(arr[0], 1L, Long::sum);
+            dailyDistribution.merge(arr[1], 1L, Long::sum);
         });
 
-        events.stream().map(event -> event.getClickedAt().getDayOfWeek().name())
-        .collect(Collectors.groupingBy(
-            day -> day,
-            Collectors.counting()
-        ))
-        .forEach((day, count) -> {
-            if (dailyDistribution.containsKey(day))
-                dailyDistribution.put(day, count);
-        });
+        // events.stream().map(event -> {
+        //     LocalDateTime dateTime = event.getClickedAt();
+        //     String formated = String.format("%02d", dateTime.getHour());
+        //     return formated;
+        // }).collect(Collectors.groupingBy(
+        //     hour -> hour,
+        //     Collectors.counting()
+        // ))
+        // .forEach((hour, count) -> {
+        //     if (hourlyDistribution.containsKey(hour))
+        //         hourlyDistribution.put(hour, count);
+        // });
+
+        // events.stream().map(event -> event.getClickedAt().getDayOfWeek().name())
+        // .collect(Collectors.groupingBy(
+        //     day -> day,
+        //     Collectors.counting()
+        // ))
+        // .forEach((day, count) -> {
+        //     if (dailyDistribution.containsKey(day))
+        //         dailyDistribution.put(day, count);
+        // });
         
 
         ClickDistributionResponse response = new ClickDistributionResponse(link.getShortCode(), hourlyDistribution, dailyDistribution);
